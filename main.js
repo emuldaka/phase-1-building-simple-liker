0abd9b905986202b64d74228a2d18680855f2c03
Elis <emuldaka@gmail.com>
2023-02-28 19:22:05 -0600

    done

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
 
+const likeGlyphs = document.querySelectorAll(".like-glyph");
+const errorModal = document.getElementById("modal");
 
+likeGlyphs.forEach((glyph) => {
+  glyph.addEventListener("click", () => {
+    mimicServerCall()
+      .then((response) => {
+        glyph.classList.toggle("activated-heart");
+      })
+      .catch((error) => {
+        errorModal.className = "";
 
+        setTimeout(() => {
+          errorModal.className = "hidden";
+        }, 3000);
+      });
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
